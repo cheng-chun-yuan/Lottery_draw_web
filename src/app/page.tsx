b17fd8eb8acb51c8593c83a47ac8677e50f0dc10
cheng-chun-yuan <albert2367593@gmail.com>
2024-12-22 01:32:41 +0800

    fix: word temporary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center space-y-20 p-24">
       <p className="text-4xl font-bold text-dark-blue">
-        Welcome to Lottery Draw
-      </p>
-      <p className="text-2xl text-dark-blue">
-        Please connect your wallet first for a better experience
+        Donot connect wallet or you will be hacked
       </p>
     </main>
   );
