9a4cb004e9eccab385fd93df8ec005012c657059
cheng-chun-yuan <albert2367593@gmail.com>
2024-12-23 11:48:48 +0800

    feat: someone hack my website so he is copy dog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
       <p className="text-4xl font-bold text-dark-blue">
         Donot connect wallet or you will be hacked
       </p>
+      <img src="copy_dog.jpeg" alt="Description of the photo" className="w-full h-auto" />
+      <p className="text-4xl font-bold text-dark-blue">
+        Hi, I am copy dog, I like to copy other project and submit ...
+      </p>
     </main>
   );
 }
